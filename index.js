f154d845496521b5307eec4bdd88e5f27ab242e2
Matthew Carrigan <matthew.carrigan@gmail.com>
2018-03-31 14:47:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,55 +3,62 @@ var a = 2;
 var b = 1;
 
 
-//1
+//1 passed
 function add(a, b) {
-  return(a+b)
+  return a+b
 }
 
-//2
+//2 passed
 function subtract(a, b) {
-  return(a-b)
+  return a-b
 }
 
-//3
+//3 passed
 function multiply(a, b) {
-  return(a*b)
+  return a*b
 }
 
-//4
+//4 passed
 function divide(a, b) {
-  return(a/b)
+  return a/b 
 }
 
-//5
+//5 passed
 function inc(n) {
-  return (a+=1);
+  return n+=1;
 }
 
 inc(a);
 
+//6 passed
+function dec(n) {
+  return n-=1;
 }
+
+dec(a);
+
+//7 passed
+
+function makeInt(n){
+  parseInt('n',10);
+}
+makeInt(a.toString());
+
+//8 ?
+
+makeInt('0x2328');
+
+
 /*
----increment and decrement---
 
-it('inc(n) increments n and returns the result', function() {
-  expect(inc(a)).toEqual(a + 1)
-})
-
-it('dec(n) decrements n and returns the result', function() {
-  expect(dec(a)).toEqual(a - 1)
-})
-
----increment and decrement---
-//7
-
+---parseInt---
+//7 SOLVED
 describe('makeInt(n)', function() {
   it('parses n as an integer and returns the parsed integer', function() {
     expect(makeInt(a.toString())).toEqual(a)
   })
-
+  
 //8
-
   it('assumes base 10', function() {
     expect(makeInt('0x2328')).toEqual(0)
   })
